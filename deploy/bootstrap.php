f33cad78cb90654c6a5d515726c288e8a7bba8a3
HAMMBE <benjamin.hamm7@gmail.com>
2022-12-15 14:47:27 +0100

    feat: Added DB

--- deploy/bootstrap.php
+++ deploy/bootstrap.php
@@ -6,11 +6,11 @@ require_once "vendor/autoload.php";
 $isDevMode = true;
 $config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
 $conn = array(
-'host' => 'host',
+'host' => 'dpg-cedhoasgqg45ht9oieqg-a',
 'driver' => 'pdo_pgsql',
-'user' => 'cnam_user',
-'password' => 'pass',
-'dbname' => 'cnam',
+'user' => 'aqua',
+'password' => 'InzDgXVxzi175Yxzn3auhkITM9K0DOj6',
+'dbname' => 'tp_hamm_benjamin_db',
 'port' => '5432'
 );
 $entityManager = EntityManager::create($conn, $config);
